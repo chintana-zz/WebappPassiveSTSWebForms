1c493d05ca05eaedb680bedb9395ee0c394b2065
Chintana Wilamuna <chintanaw@gmail.com>
2016-07-26 17:49:21 -0700

    Initial commit

---
+++ WebappPassiveSTSWebForms/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace WebappPassiveSTSWebForms.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ WebappPassiveSTSWebForms/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebappPassiveSTSWebForms.Startup))]
+namespace WebappPassiveSTSWebForms
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
